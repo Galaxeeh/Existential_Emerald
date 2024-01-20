8dc566496117adf48c701c9b3a2460fd5c30e707
Galaxeeh <kevin.a.mitchell@okstate.edu>
2024-01-19 23:32:11 -0600

    117 changes, wand/boyfriend handled for 116/rusturf tunnel, rusturf tunnel scripts

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -777,14 +777,6 @@ EventScript_HideMrBriney::
 	setvar VAR_BRINEY_LOCATION, 0
 	return
 
-RusturfTunnel_EventScript_SetRusturfTunnelOpen::
-	removeobject LOCALID_WANDAS_BF
-	removeobject LOCALID_WANDA
-	clearflag FLAG_HIDE_VERDANTURF_TOWN_WANDAS_HOUSE_WANDAS_BOYFRIEND
-	clearflag FLAG_HIDE_VERDANTURF_TOWN_WANDAS_HOUSE_WANDA
-	setvar VAR_RUSTURF_TUNNEL_STATE, 6
-	setflag FLAG_RUSTURF_TUNNEL_OPENED
-	return
 
 EventScript_UnusedBoardFerry::
 	delay 30
