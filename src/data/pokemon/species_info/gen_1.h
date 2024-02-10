3528a95e42ed424f5a63daedf3afe5d5d84582b4
Galaxeeh <kevin.a.mitchell@okstate.edu>
2024-02-10 09:32:02 -0600

    spearow changes

--- src/data/pokemon/species_info/gen_1.h
+++ src/data/pokemon/species_info/gen_1.h
@@ -1519,7 +1519,7 @@ const struct SpeciesInfo gSpeciesInfoGen1[] =
         ICON(Spearow, 0),
         .footprint = gMonFootprint_Spearow,
         LEARNSETS(Spearow),
-        .evolutions = EVOLUTION({EVO_LEVEL, 20, SPECIES_FEAROW}),
+        .evolutions = EVOLUTION({EVO_LEVEL, 17, SPECIES_FEAROW}),
     },
 
     [SPECIES_FEAROW] =
